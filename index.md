7d42327275763b0de968153fa74f1dd2132e9e2d
fxzer <fxzer8888@163.com>
2024-07-01 23:10:03 +0800

    build: init build script

---
+++ index.md
@@ -0,0 +1,11 @@
+# 安装到根目录
+
+```zsh
+ni -w xxx
+```
+
+# 为指定包安装本地依赖
+
+```zsh
+ni @vue/shared --workspace --filter @vue/reactivity
+```
---
+++ scripts/dev.js
@@ -0,0 +1,36 @@
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
+import { createRequire } from 'node:module'
+import process from 'node:process'
+import esbuild from 'esbuild'
+
+const args = process.argv.slice(2)
+
+const __dirname = resolve(fileURLToPath(import.meta.url), '..')
+const require = createRequire(import.meta.url)
+
+const target = args[0] || 'reactivity'
+
+const format = args[2] || 'iife'
+console.log('[ format ]-15', format)
+
+// 入口文件
+const entry = resolve(__dirname, `../packages/${target}/src/index.ts`)
+// 包信息
+const pkg = require(resolve(__dirname, `../packages/${target}/package.json`))
+
+// 打包
+esbuild.context({
+  entryPoints: [entry],
+  outfile: resolve(__dirname, `../packages/${target}/dist/${target}.js`),
+  bundle: true, // reactivity --> shared 会整合到一起
+  format,
+  globalName: pkg.buildOptions.name,
+  sourcemap: true,
+  platform: 'browser', // 给浏览器用的
+}).then((ctx) => {
+  console.log(`Build ${target} successfully`)
+  return ctx.watch() // 监听文件变化持续打包
+}).catch(() => {
+  console.error(`Build ${target} failed`)
+})
