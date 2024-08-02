f3690c86c19767d8066d3d085b7da45d389e3445
fxzer <fxzer8888@163.com>
2024-08-02 15:43:18 +0800

    fix: deep reactive

--- packages/reactivity/src/baseHandler.ts
+++ packages/reactivity/src/baseHandler.ts
@@ -1,4 +1,6 @@
+import { isObject } from '@vue/shared'
 import { track, trigger } from './reactiveEffect'
+import { reactive } from './reactive'
 
 export enum ReactiveFlags {
   IS_REACTIVE = '__v_isReactive',
@@ -10,12 +12,16 @@ export const mutableHandlers = {
     if (key === ReactiveFlags.IS_REACTIVE)
       return true
     track(target, key)
-    return Reflect.get(target, key)
+    const result = Reflect.get(target, key, receiver)
+    if (isObject(result)) {
+      return reactive(result) // 懒代理，若访问的值是对象，继续代理
+    }
+    return result
   },
   set(target, key, value, receiver) {
     // 避免代理对象中访问代理对象属性（ receiver[key] = value ） 触发 set，导致死循环
     const oldValue = target[key]
-    const result = Reflect.set(target, key, value)
+    const result = Reflect.set(target, key, value, receiver)
 
     // 修改前后值不相等，则触发依赖更新页面
     if (result && oldValue !== value) {
